24de3137a6a34183048036a128f7a190742e5b20
yszhou <987912102@qq.com>
2022-06-25 16:41:41 +0800

    try fix quickjs msvc

--- packages/q/quickjs/xmake.lua
+++ packages/q/quickjs/xmake.lua
@@ -10,10 +10,11 @@ package("quickjs")
         add_syslinks("pthread", "dl", "m")
     elseif is_plat("android") then
         add_syslinks("dl", "m")
-    end
-
-    if is_plat("windows") then
-        add_patches("2021.03.27", path.join(os.scriptdir(), "patches", "2021.03.27", "fix_msvc.patch"), "2b4bd5adbd6fd4a87043ba99c5c70e9c2ce842db4787fefcb1ab81012b477e30")
+    elseif is_plat("windows") then
+        add_deps("premake5")
+        add_syslinks("ws2_32")
+        -- add_syslinks("libucrt")
+        add_patches("2021.03.27", path.join(os.scriptdir(), "patches", "2021.03.27", "fix_msvc.patch"), "17377b926d4d9c95566c17cc1d02c388b3113f2a318dfe4f4e806b313cda07c5")
     end
     
     on_install("linux", "macosx", "iphoneos", "android", "mingw", "cross", function (package)
@@ -43,10 +44,6 @@ package("quickjs")
     end)
 
     on_install("windows", function (package)
-        local configs = {}
-        if package:config("shared") then
-            configs.kind = "shared"
-        end
         local vs = import("core.tool.toolchain").load("msvc"):config("vs")
         local msvc_version
         if tonumber(vs) == 2019 then
@@ -56,8 +53,10 @@ package("quickjs")
         else
             raise("unsupported msvc version " .. msvc_version)
         end
-        -- premake generate sln
-        io.writefile("premake5.lua", [[
+        local kind = package:config("shared") and "SharedLib" or "StaticLib"
+        local arch = package:is_arch("x86") and "x86_64" or "x64"
+        -- premake5.lua
+        io.writefile("premake5.lua", ([[
             function GenerateCArray(srcFile, dstFile, arrayName, arraySizeName)
                 local src = io.readfile(srcFile)
                 if src == nil then return false end
@@ -75,7 +74,7 @@ package("quickjs")
             
                 for i = 1, #src do
                     local byte = src:byte(i, i)
-                    line = line .. string.format("0x%02x, ", byte)
+                    line = line .. string.format("0x%%02x, ", byte)
             
                     if (#line >= 80) or (i == #src) then
                         if i == #src then line = line:sub(1, #line - 2) end
@@ -86,7 +85,7 @@ package("quickjs")
                 end
             
                 dst = dst .. "};\n\n"
-                dst = dst .. string.format("const unsigned " .. arraySizeName .. " = %d;\n\n", #src)
+                dst = dst .. string.format("const unsigned " .. arraySizeName .. " = %%d;\n\n", #src)
             
                 local oldDst = ""
                 if os.isfile(dstFile) then oldDst = io.readfile(dstFile) end
@@ -105,7 +104,7 @@ package("quickjs")
                 location(path.join(".build", _ACTION))
             
                 -- Target architecture
-                architecture "x86_64"
+                architecture "%s"
             
                 -- Configuration settings
                 configurations { "Debug", "Release" }
@@ -132,7 +131,7 @@ package("quickjs")
                     end
             
                 filter { }
-                    targetdir ".bin/%{cfg.longname}/"
+                    targetdir "bin/%%{cfg.longname}/"
                     defines { "WIN32", "_AMD64_", "__x86_64__" }
                     exceptionhandling "Off"
                     rtti "Off"
@@ -142,34 +141,40 @@ package("quickjs")
             
             project "quickjs"
                 language "C"
-                kind "StaticLib"
+                kind "%s"
                 files {
                     "cutils.c",
-                    "libregexp.c",
-                    "libunicode.c",
-                    "quickjs.c",
-                    "quickjs-libc.c",
                     "cutils.h",
+                    "libregexp.c",
                     "libregexp.h",
                     "libregexp-opcode.h",
+                    "libunicode.c",
                     "libunicode.h",
                     "libunicode-table.h",
                     "list.h",
+                    "quickjs.c",
                     "quickjs.h",
                     "quickjs-atom.h",
+                    "quickjs-libc.c",
                     "quickjs-libc.h",
                     "quickjs-opcode.h"
                 }
             
-        ]])
+        ]]):format(arch, kind))
+        -- premake generate sln 
         os.exec("premake5 " .. msvc_version)
         local configuration = package:debug() and "Debug" or "Release"
         -- msvc compile and build
-        import("package.tools.msbuild").build(package, {".build/" .. msvc_version .. "/quickjs.sln", "/p:platform=" .. os.arch(), "/p:configuration=" .. configuration})
-        os.mv("*.h", package:installdir("include"))
-        -- os.mv("include/*.h", package:installdir("include"))
-        -- os.mv(configuration .. "/*.lib", package:installdir("lib"))
-        -- os.mv(configuration .. "/*.dll", package:installdir("lib"))
+        local configs = {".build/" .. msvc_version .. "/quickjs.sln"}
+        table.insert(configs, "/p:platform=" .. os.arch())
+        table.insert(configs, "/p:configuration=" .. configuration)
+        import("package.tools.msbuild").build(package, configs)
+        os.mv("quickjs.h", package:installdir("include"))
+        os.trymv("bin/" .. configuration .. "/*.lib", package:installdir("lib"))
+        os.trymv("bin/" .. configuration .. "/*.dll", package:installdir("lib"))
+        if not package:config("shared") then 
+            os.tryrm(package:installdir("bin/*.dll"))
+        end
     end)
 
     on_test(function (package)
